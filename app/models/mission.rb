617d8bd0ac5a10c31afc2c11721ef88faa356132
Lawa0921 <bag571ivy3470@gmail,com>
2020-06-19 13:14:49 +0800

    中轉英 review 調整

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,4 @@
 class Mission < ApplicationRecord
-  enum status: {"待處理": 0, "進行中": 1, "已完成": 2}
-  enum order: {"低": 0, "中": 1, "高": 2}
+  enum status: %i(pending working done)
+  enum order: %i(low medium high)
 end
