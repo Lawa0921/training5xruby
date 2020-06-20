33a0ae793a454056216b581d994099991d36f8fc
Lawa0921 <bag571ivy3470@gmail,com>
2020-06-20 20:56:18 +0800

    mission_spec調整 移除不必要 class

--- spec/features/missions_spec.rb
+++ spec/features/missions_spec.rb
@@ -1,10 +1,9 @@
 require "rails_helper"
 RSpec.feature "Missions", type: :feature do
-  before (:all )do
-    create(:user)
-  end
+  let (:user) { create(:user)}
+  let (:mission) { create(:mission)}
 
-  scenario "可新增任務" do
+  scenario "可新增 mission" do
     visit root_path
     click_link 'new mission'
 
@@ -17,24 +16,26 @@ RSpec.feature "Missions", type: :feature do
   end
 
   scenario "可以刪除 mission" do
-    create(:mission, name: "m1")
+    mission.name = "m1"
+    mission.save
     visit root_path
     expect(page).to have_content "m1"
+    
     click_link 'delete'
 
     expect(page).not_to have_content 'm1'
   end
 
   scenario "可以編輯 mission" do
-    create(:mission, name: "m1")
+    mission.name = "m1"
+    mission.save
     visit root_path
     expect(page).to have_content 'm1'
+
     click_link 'edit'
     page.fill_in "mission[name]", with: "m2"
     page.fill_in "mission[description]", with: "m2"
     click_button 'Update Mission'
-
-    visit root_path
 
     expect(page).not_to have_content 'm1'
     expect(page).to have_content 'm2'
