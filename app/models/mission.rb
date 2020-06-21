3324a2eb199cf0b340d958fe36a81ab8966762a8
Lawa0921 <bag571ivy3470@gmail,com>
2020-06-21 20:53:52 +0800

    加入 model 驗證

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,4 +2,6 @@ class Mission < ApplicationRecord
   enum status: [I18n.t('mission.pending'), I18n.t('mission.working'), I18n.t('mission.done')]
   enum order: [I18n.t("mission.low"), I18n.t("mission.medium"), I18n.t("mission.high")]
   has_many :tags, through: :mission_tag
+
+  validates :name, :status, :order, :start_at, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
   has_many :missions, through: :mission_tag
+  validates :name, presence: true,
+                   uniqueness: true
 end
