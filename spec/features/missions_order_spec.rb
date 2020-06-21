3863152b7a1580abda11cc8029737fb925620f5f
Lawa0921 <bag571ivy3470@gmail,com>
2020-06-21 20:06:13 +0800

    引入 Timecop 調整測試

--- spec/features/missions_order_spec.rb
+++ spec/features/missions_order_spec.rb
@@ -3,9 +3,11 @@ RSpec.feature "Missions Order", type: :feature do
   describe "#index" do
     context "Order by created_at" do
       before do
-        create(:mission, created_at: 1.day.ago, name: "secord")
         create(:mission, created_at: Time.now, name: "first")
-        create(:mission, created_at: 2.days.ago, name: "last")
+        Timecop.travel(Time.local(2020, 6, 18, 20, 0, 0))
+        create(:mission, created_at: Time.now, name: "secord")
+        Timecop.travel(Time.local(2020, 6, 14, 10, 0, 0))
+        create(:mission, created_at: Time.now, name: "last")
         visit root_path
       end
       it "依照創建時間排序" do
