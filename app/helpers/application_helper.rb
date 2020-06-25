05f748ad653708952030aaf9d085d255817ef764
lawa0921 <bag571ivy3470@gmail>
2020-06-25 22:10:58 +0800

    拆解 model method 至 helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,12 @@ module ApplicationHelper
     I18n.t("mission.priorities.#{priority}")
   end
 
-  def human_enum_name(model, attr)
-    I18n.t("#{model.model_name.i18n_key}.#{attr.to_s}.#{model.send(attr)}")
+  def enum_name(model_name, enum_name, enum_value)
+    I18n.t("#{model_name}.#{enum_name.to_s.pluralize}.#{enum_value}")
+  end
+
+  def enum_collection(model_name, enum_name)
+    model = model_name.capitalize.constantize
+    model.send(enum_name.to_s.pluralize).keys.collect { |val| [enum_name(model_name, enum_name, val), val] }
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  def self.human_enum_name(enum_name, enum_value)
-    I18n.t("#{model_name.singular}.#{enum_name.to_s.pluralize}.#{enum_value}")
-  end
-
-  def self.human_enum_collection(enum_name)
-    send(enum_name.to_s.pluralize).keys.collect { |val| [human_enum_name(enum_name, val), val] }
-  end
 end
