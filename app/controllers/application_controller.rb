dd77c18987fdb4d48223fd149df738b2c6278423
lawa0921 <bag571ivy3470@gmail>
2020-06-25 10:56:34 +0800

    翻譯檔調整 登入檢查準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :check_session
+
+  private
+  def check_session
+    redirect_to session_new_path if not session[:login_session] 
+  end
+
+  def current_user
+    @user ||= User.find_by(id: session[:login_session])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :check_session
+
   def new
     @user = User.new
   end
