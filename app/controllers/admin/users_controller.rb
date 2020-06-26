3f5331ae38ad7d93d7837fe797202fdc8a09c7c8
lawa0921 <bag571ivy3470@gmail>
2020-06-26 13:15:26 +0800

    admin show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,10 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all.page(params[:page]).per(5)
   end
+
+  def show
+    @user = User.find(params[:id])
+    @q = @user.missions.ransack(params[:q])
+    @missions = @q.result.with_order(params[:order_by]).page(params[:page]).per(5).includes(:user)
+  end
 end
