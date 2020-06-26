d5c383cfdad138bf194363b2ac50cd22c3224f6f
lawa0921 <bag571ivy3470@gmail>
2020-06-26 13:37:19 +0800

    admin new and create

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,4 +8,18 @@ class Admin::UsersController < ApplicationController
     @q = @user.missions.ransack(params[:q])
     @missions = @q.result.with_order(params[:order_by]).page(params[:page]).per(5).includes(:user)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: t("notice.user.create_success")
+    else
+      flash[:notice] = t('notice.user.create_fail')
+      render :new
+    end
+  end
 end
