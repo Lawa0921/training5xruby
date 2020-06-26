6ab6a2919677ca3be8c09e643968b5f8ece7d323
lawa0921 <bag571ivy3470@gmail>
2020-06-26 13:51:45 +0800

    admin edit and update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,11 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :destroy, :update]
+
   def index
     @users = User.all.page(params[:page]).per(5)
   end
 
   def show
-    @user = User.find(params[:id])
     @q = @user.missions.ransack(params[:q])
     @missions = @q.result.with_order(params[:order_by]).page(params[:page]).per(5).includes(:user)
   end
@@ -20,6 +21,18 @@ class Admin::UsersController < ApplicationController
     else
       flash[:notice] = t('notice.user.create_fail')
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: t('users.update')
+    else
+      flash[:notice] = t('users.update_failure')
+      render :edit
     end
   end
 end
